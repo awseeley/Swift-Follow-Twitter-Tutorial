6cd75a89a840ef240f108c6cb4fa2b86718cf254
Andrew Seeley <awseeley@gmail.com>
2014-12-01 21:36:41 +1100

    First add
    First Add

--- OpenTwitterPage/ViewController.swift
+++ OpenTwitterPage/ViewController.swift
@@ -1,10 +1,7 @@
 //
 //  ViewController.swift
 //  OpenTwitterPage
-//
-//  Created by Andrew Seeley on 1/12/2014.
-//  Copyright (c) 2014 Seemu. All rights reserved.
-//
+
 
 import UIKit
 
@@ -20,6 +17,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func openTwitterPage(sender: UIButton) {
+        
+        var twUrl: NSURL = NSURL(string: "twitter://user?screen_name=SeemuApps")
+        var twUrlWeb: NSURL = NSURL(string: "https://twitter.com/SeemuApps")
+        
+        if(UIApplication.sharedApplication().canOpenURL(twUrl)) {
+            // If user twitter installed
+            UIApplication.sharedApplication().openURL(twUrl)
+        } else {
+            // If user does not have twitter installed
+            UIApplication.sharedApplication().openURL(twUrlWeb)
+        }
+        
+    }
 
 }
 
